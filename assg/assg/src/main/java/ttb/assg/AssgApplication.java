ba36a485bf9dff9c9157904bcee119dc34bfa751
Nattapan Meepiean <formulae445@gmail.com>
2025-02-22 17:20:28 +0700

    ✨ feat(init) :  init project - Initial commit of the assg project. This includes the creation of the project structure, configuration files (pom.xml, application.properties), and basic Spring Boot application code

---
+++ assg/assg/src/main/java/ttb/assg/AssgApplication.java
@@ -0,0 +1,13 @@
+package ttb.assg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssgApplication.class, args);
+	}
+
+}
---
+++ assg/assg/src/test/java/ttb/assg/AssgApplicationTests.java
@@ -0,0 +1,13 @@
+package ttb.assg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
